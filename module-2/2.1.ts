8c4c6043cbf1f315f9f1761bfeb62a69441c04f0
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-11-04 11:22:13 +0100

    Revision of typeScript concept

---
+++ module-2/2.1.ts
@@ -0,0 +1,29 @@
+{
+let anyThing : any ;
+anyThing = "Next level Web Development";
+
+(anyThing as string )
+
+
+const kgToGm = (value : number | string | undefined )=> {
+    if (typeof value === "number"){
+        const convertedValue = value * 1000;
+        return convertedValue;
+    }
+
+    if (typeof value === "string"){
+        const parseValue = parseFloat(value)*1000;
+        return parseValue
+    }
+
+}
+
+kgToGm(1000 as number)
+kgToGm("500" as string)
+
+
+
+
+
+
+}
