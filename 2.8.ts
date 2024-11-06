5f0d85a74cfb1f4c476b1d2ab949ed23c51c18f8
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-11-06 14:05:35 +0100

    done with async typeScript

--- 2.8.ts
+++ 2.8.ts
@@ -1,28 +1,19 @@
 {
+  // asyncronous operations
 
-    // asyncronous operations 
+  const createPromise = () => {
+    return new Promise((resolve, reject) => {
+      const data: string = "something";
+      if (data) {
+        resolve(data);
+      } else {
+        reject("Failed to load Data");
+      }
+    });
+  };
 
-    const createPromise = ()=> {
-        return new Promise((resolve, reject)=> {
-            const data : string = "something";
-            if(data){
-                resolve(data)
-            }
-            else {
-                reject ("Failed to load Data")
-            }
-        })
-    }
-
-
-
-
-
-
-
-
-
-
-
-
+  const showData = async () => {
+    const data = await createPromise();
+    return data 
+  };
 }
