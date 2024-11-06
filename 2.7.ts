3dc6859532aa7a5ec2e107fb0731184bfa5251f8
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-11-06 12:24:43 +0100

    generic with keyof operators

--- 2.7.ts
+++ 2.7.ts
@@ -15,6 +15,23 @@ type Owner = "bike"| "car"| "ship";
 type Owner2 = keyof  Vehicle
 
 
+const user = {
+    name: "Mr. Persian",
+    age: 20,
+    email: "@gmail.com" 
 
+}
+
+const car = {
+    brand: "Toyota",
+    model: 2020
+}
+
+const getPropertyValue =<X,Y extends keyof X> (object: X, key: Y )=> {
+    return object[key]
+}
+
+const result1 = getPropertyValue(user, "name")
+const result2 = getPropertyValue(car, "model")
 
 }
