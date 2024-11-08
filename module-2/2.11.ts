567f6507c2a52f0e44b4af74752ed29487333c03
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-11-08 13:43:36 +0100

    done with class constractor method and instance example

--- module-2/2.11.ts
+++ module-2/2.11.ts
@@ -6,9 +6,23 @@
     contactNo: string;
   };
 
+//   pick utility 
+
   type Name = Pick<Person, "name">;
 
   type NameAge = Pick<Person, "name" | "age">;
+// Omit Utility 
 
-  
+type ContactInfo = Omit <Person , "name"| "age">
+
+
+// required Utility 
+type PersonRequired = Required <Person>
+// partial utility 
+type PersonPartial = Partial <Person>;
+
+
+
+
+
 }
