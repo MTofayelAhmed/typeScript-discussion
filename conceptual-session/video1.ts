d32320fb5a99c4baeba441f00ce5dae534740768
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-11-09 10:57:49 +0100

    done with typeof typeGuard

--- conceptual-session/video1.ts
+++ conceptual-session/video1.ts
@@ -11,19 +11,50 @@ type BookType = keyof Book
 
 function getPropertyValue <T,K extends keyof T>(book: T, key: K): T[K]{
 return book[key]
+}
+// type of operator 
 
 
+const Movie = "Deadpool";
+type MovieName = typeof Movie;
 
 
+const MovieName = {
+    title: "Rock On",
+    director: "Md Tofayel Ahmed"
+    
+}
+
+
+type MovieInfo = typeof MovieName;
+
+
+const movieDetails: MovieInfo = {
+title: "NoteBook",
+director: "Mr. Ramiz"
 }
 
 
 
 
+type DetailLogs = {
+    message: string;
+    level: "info"| "warn"| "error"
+}
 
 
 
-
+function messageType (msg: string| number | DetailLogs){
+    if(typeof msg === "string"){
+        console.log(`the message is ${msg}`)
+    }
+    else if (typeof msg === "number"){
+        console.log()
+    }
+    else {
+        console.log()
+    }
+}
 
 
 }
